060e380722a48b977d99a605fb4ceb9ab4000d25
abdennour <mail@abdennoor.com>
2018-02-13 08:58:04 +0300

    Beautify the code according to prettier rules.

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,53 +1,66 @@
 import React from 'react';
-import {buildURI, toCSV} from '../core';
+import { buildURI, toCSV } from '../core';
 import {
-   defaultProps as commonDefaultProps,
-   propTypes as commonPropTypes} from '../metaProps';
+  defaultProps as commonDefaultProps,
+  propTypes as commonPropTypes
+} from '../metaProps';
 
 /**
  *
  * @example ../../sample-site/csvlink.example.md
  */
 class CSVLink extends React.Component {
-
   static defaultProps = commonDefaultProps;
   static propTypes = commonPropTypes;
 
   constructor(props) {
     super(props);
-    this.buildURI= this.buildURI.bind(this);
+    this.buildURI = this.buildURI.bind(this);
   }
 
   buildURI() {
     return buildURI(...arguments);
   }
 
-/**
+  /**
    * In IE11 this method will trigger the file download
    */
   handleLegacy(evt, data, headers, separator, filename) {
     // If this browser is IE 11, it does not support the `download` attribute
     if (window.navigator.msSaveOrOpenBlob) {
       // Stop the click propagation
-      evt.preventDefault()
+      evt.preventDefault();
 
-      let blob = new Blob([toCSV(data, headers, separator)])
-      window.navigator.msSaveBlob(blob, filename)
+      let blob = new Blob([toCSV(data, headers, separator)]);
+      window.navigator.msSaveBlob(blob, filename);
 
-      return false
+      return false;
     }
   }
 
-  render(){
-    const {data, headers, separator, filename, uFEFF, children , ...rest} = this.props;
+  render() {
+    const {
+      data,
+      headers,
+      separator,
+      filename,
+      uFEFF,
+      children,
+      ...rest
+    } = this.props;
     return (
-      <a download={filename} {...rest}
-         ref={link => (this.link = link)}
-         href={this.buildURI(data, uFEFF, headers, separator)}
-         onClick={evt => this.handleLegacy(evt, data, headers, separator, filename)}>
+      <a
+        download={filename}
+        {...rest}
+        ref={link => (this.link = link)}
+        href={this.buildURI(data, uFEFF, headers, separator)}
+        onClick={evt =>
+          this.handleLegacy(evt, data, headers, separator, filename)
+        }
+      >
         {children}
       </a>
-    )
+    );
   }
 }
 
