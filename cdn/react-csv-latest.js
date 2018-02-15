d19a3fc8e2ca3f0418c1a539e46970c2ccc81e32
Travis Mair <travis.mair@gmail.com>
2018-02-15 15:36:47 -0700

    Let filename for csvdownload be available

--- cdn/react-csv-latest.js
+++ cdn/react-csv-latest.js
@@ -10,15 +10,15 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
-var _core = require('../core');
-
-var _metaProps = require('../metaProps');
+var _index = require('../index');
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -28,60 +28,33 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var defaultProps = {
-  target: '_blank'
-};
-
 var CSVDownload = function (_React$Component) {
   _inherits(CSVDownload, _React$Component);
 
-  function CSVDownload(props) {
+  function CSVDownload() {
     _classCallCheck(this, CSVDownload);
 
-    var _this = _possibleConstructorReturn(this, (CSVDownload.__proto__ || Object.getPrototypeOf(CSVDownload)).call(this, props));
-
-    _this.state = {};
-    return _this;
+    return _possibleConstructorReturn(this, (CSVDownload.__proto__ || Object.getPrototypeOf(CSVDownload)).apply(this, arguments));
   }
 
   _createClass(CSVDownload, [{
-    key: 'buildURI',
-    value: function buildURI() {
-      return _core.buildURI.apply(undefined, arguments);
-    }
-  }, {
-    key: 'componentDidMount',
-    value: function componentDidMount() {
-      var _props = this.props,
-          data = _props.data,
-          headers = _props.headers,
-          separator = _props.separator,
-          uFEFF = _props.uFEFF,
-          target = _props.target,
-          specs = _props.specs,
-          replace = _props.replace;
-
-      this.state.page = window.open(this.buildURI(data, uFEFF, headers, separator), target, specs, replace);
-    }
-  }, {
-    key: 'getWindow',
-    value: function getWindow() {
-      return this.state.page;
+    key: 'handleRef',
+    value: function handleRef(ref) {
+      ref.link.click();
+      ref.link.remove();
     }
   }, {
     key: 'render',
     value: function render() {
-      return null;
+      return _react2.default.createElement(_index.CSVLink, _extends({ ref: this.handleRef }, this.props));
     }
   }]);
 
   return CSVDownload;
 }(_react2.default.Component);
 
-CSVDownload.defaultProps = Object.assign(_metaProps.defaultProps, defaultProps);
-CSVDownload.propTypes = _metaProps.propTypes;
 exports.default = CSVDownload;
-},{"../core":4,"../metaProps":6,"react":44}],3:[function(require,module,exports){
+},{"../index":5,"react":44}],3:[function(require,module,exports){
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
--- src/components/Download.js
+++ src/components/Download.js
@@ -1,47 +1,18 @@
 import React from 'react';
-import {buildURI} from '../core';
-import {
-   defaultProps as commonDefaultProps,
-   propTypes as commonPropTypes} from '../metaProps';
-const defaultProps = {
-  target: '_blank'
-};
+import { CSVLink } from '../index';
 
 /**
  *
  * @example ../../sample-site/csvdownload.example.md
  */
 class CSVDownload extends React.Component {
-
-  static defaultProps = Object.assign(
-    commonDefaultProps,
-    defaultProps
-  );
-
-  static propTypes = commonPropTypes;
-
-  constructor(props) {
-    super(props);
-    this.state={};
+  handleRef(ref) {
+    ref.link.click();
+    ref.link.remove();
   }
 
-  buildURI() {
-    return buildURI(...arguments);
-  }
-
-  componentDidMount(){
-    const {data, headers, separator, uFEFF, target, specs, replace} = this.props;
-    this.state.page = window.open(
-        this.buildURI(data, uFEFF, headers, separator), target, specs, replace
-    );
-  }
-
-  getWindow() {
-    return this.state.page;
-  }
-
-  render(){
-    return (null)
+  render() {
+    return <CSVLink ref={this.handleRef} {...this.props} />;
   }
 }
 
