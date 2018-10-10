e6db715a9172a6da34225f1754aeae6c1f15fa14
gion <bogdan.gradinariu@gmail.com>
2018-10-10 15:20:44 +0300

    Restore app data

--- sample-site/src/app.jsx
+++ sample-site/src/app.jsx
@@ -3,10 +3,17 @@ import {CSVLink, CSVDownload} from 'react-csv';
 import Table from './Table.jsx';
 
 const csvHeaders = [
-"a", "b", "c"
+"Company","جهة الإتصال ","王玉普"
 ]
 const csvData =[
   ['Alfreds Futterkiste'	,'Maria Anders',	'Germany'] ,
+  ['Rathath IT', 'Abdennour TM' , 'تونس'] ,
+  ['Sinopec', '王玉普' , '中国'],
+  ['Auto1', 'Petter' , 'Germany'] ,
+  ['Estifeda', 'Yousri K' , 'تونس'] ,
+  ['Nine 10ᵗʰ', 'Amjed Idris' , 'المملكة العربية السعودية '] ,
+  ['Tamkeen', 'Mohamed Alshibi' , 'المملكة العربية السعودية'] ,
+  ['Packet Publishing', 'David Become' , 'UK'] ,
   ['Software hourse', 'Soro' , 'Poland']
 ];
 
@@ -34,7 +41,6 @@ class App extends React.Component {
               </div>
               <div className="large-2 columns">
                   <CSVLink
-                    target="_self"
                     headers={csvHeaders}
                     data={csvData}
                     filename={this.getFileName()}
