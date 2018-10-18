ba6613fa127241b36a4e28c2940f0c6ef5fc335b
Jarvis1010 <travis.mair@gmail.com>
2018-10-18 15:38:12 -0600

    update merge conflicts and test

--- test/ComponentsSpec.js
+++ test/ComponentsSpec.js
@@ -1,219 +1,157 @@
-import jsdom from 'jsdom-global';
-import React from 'react';
-import {
- mount,
- shallow
-} from 'enzyme';
-import sinon from 'sinon';
-import expect from 'expect';
+import jsdom from "jsdom-global";
+import React from "react";
+import { mount, shallow } from "enzyme";
+import sinon from "sinon";
+import expect from "expect";
 
-import {
- CSVLink,
- CSVDownload
-} from '../src';
-import {buildURI} from '../src/core';
-import 'console-info';
+import { CSVLink, CSVDownload } from "../src";
+import { buildURI } from "../src/core";
+import "console-info";
 
+const getAttrs = htmlElment =>
+  Array.from(htmlElment.attributes).reduce((a, b) => {
+    a[b.name === "class" ? "className" : b.name] = b.value;
+    return a;
+  }, {});
 
-const getAttrs =((htmlElment) => Array.from(
-    htmlElment.attributes
-  ).reduce((a, b) => {
-      a[b.name ==='class' ? 'className':b.name ] = b.value ;
-      return a;}
-   , {})
-);
+describe("In browser environment", () => {
+  before(function() {
+    this.jsdom = require("jsdom-global")();
+  });
 
-describe('In browser environment', () => {
-  before(function () {
-    this.jsdom = require('jsdom-global')()
-  })
-  
-  after(function () {
-    this.jsdom()
-  })
+  after(function() {
+    this.jsdom();
+  });
 
-   it(`assigns a download filename`, () => {
-     const filename= "persons.csv";
-     const wrapper = mount( <CSVLink {...minProps} filename={filename} > here </CSVLink>);
-     expect(wrapper.find('a').get(0).getAttribute('download')).toEqual(filename);
-   });
+  describe("CSVLink", () => {
+    let minProps;
+    beforeEach(() => {
+      minProps = {
+        data: [["X", "Y"], ["1", "2"], ["3", "4"]],
+      };
+    });
+
+    it(`renders without error if required props are passed`, () => {
+      const wrapper = shallow(<CSVLink {...minProps}> Click here </CSVLink>);
+      expect(wrapper.length).toEqual(1);
+    });
+
+    it(`has comma as default separator `, () => {
+      const wrapper = mount(<CSVLink {...minProps}> Click here </CSVLink>);
+      expect(wrapper.props().separator).toEqual(",");
+    });
+
+    it(`assigns a download filename`, () => {
+      const filename = "persons.csv";
+      const wrapper = mount(
+        <CSVLink {...minProps} filename={filename}>
+          {" "}
+          here{" "}
+        </CSVLink>
+      );
+      expect(
+        wrapper
+          .find("a")
+          .get(0)
+          .getAttribute("download")
+      ).toEqual(filename);
+    });
 
     it(`renders anchor tag`, () => {
-      const wrapper = shallow( <CSVLink {...minProps} > Click here </CSVLink>);
-       expect(wrapper.find('a').length).toEqual(1);
-     });
-
-
+      const wrapper = shallow(<CSVLink {...minProps}> Click here </CSVLink>);
+      expect(wrapper.find("a").length).toEqual(1);
+    });
 
     it(`calls "buildURI" method on mounting`, () => {
-      const dataURI = `data:text/csv;some,thing`
-      const buildURI = sinon.stub(CSVLink.prototype, 'buildURI').returns(dataURI);
-      const wrapper = mount( <CSVLink {...minProps} > Click here </CSVLink>);
+      const dataURI = `data:text/csv;some,thing`;
+      const buildURI = sinon
+        .stub(CSVLink.prototype, "buildURI")
+        .returns(dataURI);
+      const wrapper = mount(<CSVLink {...minProps}> Click here </CSVLink>);
       expect(buildURI.calledOnce).toBeTruthy();
       buildURI.restore();
     });
     it(`generates CSV download link and bind it to "href" of <a> element`, () => {
-      const linkPrefix = `data:text/csv`
-      const wrapper = mount( <CSVLink {...minProps} > Click here </CSVLink>);
-      const actualLink = wrapper.find(`a`).get(0).getAttribute('href');
+      const linkPrefix = `data:text/csv`;
+      const wrapper = mount(<CSVLink {...minProps}> Click here </CSVLink>);
+      const actualLink = wrapper
+        .find(`a`)
+        .get(0)
+        .getAttribute("href");
       expect(actualLink.startsWith(linkPrefix)).toBeTruthy();
     });
 
-     it(`forwards props to anchor tag unless props is forbidden`, () => {
+    it(`forwards props to anchor tag unless props is forbidden`, () => {
       const extraProps = {
-        className:`btn`,
-        target:'_self'
+        className: `btn`,
+        target: "_self",
       };
-      const wrapper = mount( <CSVLink {...Object.assign(minProps, extraProps)} > Click here </CSVLink>);
-      const actualAnchorAttrs =getAttrs(wrapper.find(`a`).get(0));
-     expect(actualAnchorAttrs).toInclude(extraProps);
-
-    })
+      const wrapper = mount(
+        <CSVLink {...Object.assign(minProps, extraProps)}> Click here </CSVLink>
+      );
+      const actualAnchorAttrs = getAttrs(wrapper.find(`a`).get(0));
+      expect(actualAnchorAttrs).toInclude(extraProps);
+    });
 
     it(`generates "onClick" event for IE11 support`, () => {
-      const wrapper = shallow( <CSVLink {...minProps}> here </CSVLink>);
-      wrapper.find(`a`).simulate(`click`, { preventDefault() {}})
-      expect(wrapper.find(`a`).get(0).props).toContainKey('onClick');
+      const wrapper = shallow(<CSVLink {...minProps}> here </CSVLink>);
+      wrapper.find(`a`).simulate(`click`, { preventDefault() {} });
+      expect(wrapper.find(`a`).get(0).props).toContainKey("onClick");
     });
     // TODO write unit-tests for handleClick
     // TODO write unit-tests for handleSyncClick
     // TODO write unit-tests for handleAsyncClick
-
-   });
-
-  describe('CSVLink', () => {
-    let minProps;
-    beforeEach(() => {
-     minProps = {
-      data: [
-       ['X', 'Y'],
-       ['1', '2'],
-       ['3', '4']
-      ]
-     };
-    });
- 
-    it(`renders without error if required props are passed`, () => {
-      const wrapper = shallow( <CSVLink {...minProps} > Click here </CSVLink>);
-       expect(wrapper.length).toEqual(1);
-    });
- 
-    it(`has comma as default separator `, () => {
-      const wrapper = mount( <CSVLink {...minProps} > Click here </CSVLink>);
-      expect(wrapper.props().separator).toEqual(',');
-    })
- 
-    it(`assigns a download filename`, () => {
-      const filename= "persons.csv";
-      const wrapper = mount( <CSVLink {...minProps} filename={filename} > here </CSVLink>);
-      expect(wrapper.find('a').get(0).getAttribute('download')).toEqual(filename);  
-    });
- 
-     it(`renders anchor tag`, () => {
-       const wrapper = shallow( <CSVLink {...minProps} > Click here </CSVLink>);
-        expect(wrapper.find('a').length).toEqual(1);
-      });
- 
- 
- 
-     it(`calls "buildURI" method on mounting`, () => {
-       const dataURI = `data:text/csv;some,thing`
-       const buildURI = sinon.stub(CSVLink.prototype, 'buildURI').returns(dataURI);
-       const wrapper = mount( <CSVLink {...minProps} > Click here </CSVLink>);
-       expect(buildURI.calledOnce).toBeTruthy();
-       buildURI.restore();
-     });
-     it(`generates CSV download link and bind it to "href" of <a> element`, () => {
-       const linkPrefix = `data:text/csv`
-       const wrapper = mount( <CSVLink {...minProps} > Click here </CSVLink>);
-       const actualLink = wrapper.find(`a`).get(0).getAttribute('href');
-       expect(actualLink.startsWith(linkPrefix)).toBeTruthy();
-     });
- 
-      it(`forwards props to anchor tag unless props is forbidden`, () => {
-       const extraProps = {
-         className:`btn`,
-         target:'_self'
-       };
-       const wrapper = mount( <CSVLink {...Object.assign(minProps, extraProps)} > Click here </CSVLink>);
-       const actualAnchorAttrs =getAttrs(wrapper.find(`a`).get(0));
-      expect(actualAnchorAttrs).toInclude(extraProps);
- 
-     })
-     
-     it(`generates "onClick" event for IE11 support`, () => {
-       const wrapper = shallow( <CSVLink {...minProps}> here </CSVLink>);
-       wrapper.find(`a`).simulate(`click`, { preventDefault() {}})
-       expect(wrapper.find(`a`).get(0).props).toContainKey('onClick');
-     });
- 
-    });
- 
-   describe('CSVDownload', () => {
-     let minProps;
-     let manyProps;
-     beforeEach(() => {
-      minProps = {
-       data: [
-        ['X', 'Y'],
-        ['1', '2'],
-        ['3', '4']
-       ],
-       uFEFF: true
-      };
-      manyProps= Object.assign(minProps, {
-       target: '_blank',
-       specs: 'fullscreen=yes&height=200&status=yes',
-       replace:'no'
-      });
-     });
- 
-     it(`does not render anything by default`, () => {
-       const wrapper = shallow( <CSVDownload {...minProps} />);
-        expect(wrapper.props().children).toNotExist();
-       });
-      it(`calls "buildURI" on mounting`, () => {
-        const dataURI = `data:text/csv;some,thing`
-        const buildURI = sinon.stub(CSVDownload.prototype, 'buildURI').returns(dataURI);
-        const wrapper = mount( <CSVDownload {...minProps} />);
-        expect(buildURI.calledOnce).toBeTruthy();
-        buildURI.restore();
-      });
-      it(`redirects in different page on mounting`, () => {
-        const openCallback = sinon.stub(window,'open').returns({
-          focus: ()=> {}
-        });
-        const wrapper = mount( <CSVDownload {...manyProps} />);
-        expect(openCallback.calledWith(buildURI(manyProps.data, manyProps.uFEFF), manyProps.target, manyProps.specs, manyProps.replace)).toBeTruthy();
-        expect(openCallback.calledOnce).toBeTruthy();
-        openCallback.restore();
-       });
- 
-      it(`persists new opened window`, () => {
-        const openCallback = sinon.stub(window,'open').returns('newPage');
-        const wrapper = mount( <CSVDownload {...manyProps} />);
-        const actualNewWindow= wrapper.instance().getWindow();
-        expect(actualNewWindow).toEqual('newPage');
-        openCallback.restore();
- 
-     });
   });
- 
-})
 
-describe('In Node environment', () => {
-  it('does not call buildURI', () => {
+  describe("CSVDownload", () => {
+    let minProps;
+    let manyProps;
+    beforeEach(() => {
+      minProps = {
+        data: [["X", "Y"], ["1", "2"], ["3", "4"]],
+        uFEFF: true,
+      };
+      manyProps = Object.assign(minProps, {
+        target: "_blank",
+        specs: "fullscreen=yes&height=200&status=yes",
+        replace: "no",
+      });
+    });
+
+    it(`does not render anything by default`, () => {
+      const wrapper = shallow(<CSVDownload {...minProps} />);
+      expect(wrapper.props().children).toNotExist();
+    });
+    it(`calls "handleRef" on mounting`, () => {
+      const dataURI = `data:text/csv;some,thing`;
+      const handleRef = sinon.stub(CSVDownload.prototype, "handleRef");
+      const wrapper = mount(<CSVDownload {...minProps} />);
+      expect(handleRef.calledOnce).toBeTruthy();
+      handleRef.restore();
+    });
+
+    it(`on mounting no element in dom`, () => {
+      const filename = "persons.csv";
+      const wrapper = mount(<CSVDownload {...minProps} filename={filename} />);
+      expect(
+        wrapper
+          .find("a")
+          .get(0)
+          .getAttribute("download")
+      ).toEqual(filename);
+    });
+  });
+});
+
+describe("In Node environment", () => {
+  it("does not call buildURI", () => {
     const minProps = {
-      data: [
-       ['X', 'Y'],
-       ['1', '2'],
-       ['3', '4']
-      ]
+      data: [["X", "Y"], ["1", "2"], ["3", "4"]],
     };
 
-    const dataURI = `data:text/csv;some,thing`
-    const buildURI = sinon.stub(CSVLink.prototype, 'buildURI').returns(dataURI);
-    const wrapper = shallow( <CSVLink {...minProps} > Click here </CSVLink>);
+    const dataURI = `data:text/csv;some,thing`;
+    const buildURI = sinon.stub(CSVLink.prototype, "buildURI").returns(dataURI);
+    const wrapper = shallow(<CSVLink {...minProps}> Click here </CSVLink>);
     expect(buildURI.notCalled).toBeTruthy();
     buildURI.restore();
   });
