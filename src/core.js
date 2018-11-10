bef830f2560056b5d6cb0c3ee70d977a1635769c
NandoTheessen <nando.theessen@gmail.com>
2018-11-10 17:57:39 +0100

    In core::joiner filter incoming data & perform join operations on the filtered Data instead Change tests to reflect that change

--- src/core.js
+++ src/core.js
@@ -1,20 +1,22 @@
 /**
  * Simple safari detection based on user agent test
  */
-export const isSafari = () => /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+export const isSafari = () =>
+  /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 
-export const isJsons = ((array) => Array.isArray(array) && array.every(
- row => (typeof row === 'object' && !(row instanceof Array))
-));
+export const isJsons = array =>
+  Array.isArray(array) &&
+  array.every(row => typeof row === 'object' && !(row instanceof Array));
 
-export const isArrays = ((array) => Array.isArray(array) && array.every(
- row => Array.isArray(row)
-));
+export const isArrays = array =>
+  Array.isArray(array) && array.every(row => Array.isArray(row));
 
-export const jsonsHeaders = ((array) => Array.from(
- array.map(json => Object.keys(json))
- .reduce((a, b) => new Set([...a, ...b]), [])
-));
+export const jsonsHeaders = array =>
+  Array.from(
+    array
+      .map(json => Object.keys(json))
+      .reduce((a, b) => new Set([...a, ...b]), [])
+  );
 
 export const jsons2arrays = (jsons, headers) => {
   headers = headers || jsonsHeaders(jsons);
@@ -23,18 +25,20 @@ export const jsons2arrays = (jsons, headers) => {
   let headerLabels = headers;
   let headerKeys = headers;
   if (isJsons(headers)) {
-    headerLabels = headers.map((header) => header.label);
-    headerKeys = headers.map((header) => header.key);
+    headerLabels = headers.map(header => header.label);
+    headerKeys = headers.map(header => header.key);
   }
 
-  const data = jsons.map((object) => headerKeys.map((header) => getHeaderValue(header, object)));
+  const data = jsons.map(object =>
+    headerKeys.map(header => getHeaderValue(header, object))
+  );
   return [headerLabels, ...data];
 };
 
 export const getHeaderValue = (property, obj) => {
   const foundValue = property
-    .replace(/\[([^\]]+)]/g, ".$1")
-    .split(".")
+    .replace(/\[([^\]]+)]/g, '.$1')
+    .split('.')
     .reduce(function(o, p, i, arr) {
       // if at any point the nested keys passed do not exist, splice the array so it doesnt keep reducing
       if (o[p] === undefined) {
@@ -43,48 +47,50 @@ export const getHeaderValue = (property, obj) => {
         return o[p];
       }
     }, obj);
-  
-  return (foundValue === undefined) ? '' : foundValue;
-}
 
-export const elementOrEmpty = (element) => element || element === 0 ? element : '';
-
-export const joiner = ((data,separator = ',') =>
- data.map((row, index) => {
-  if(row == null) return null
-  return row.map((element) => "\"" + elementOrEmpty(element) + "\"").join(separator)
-  }).join(`\n`)
-);
-
-export const arrays2csv = ((data, headers, separator) =>
- joiner(headers ? [headers, ...data] : data, separator)
-);
-
-export const jsons2csv = ((data, headers, separator) =>
- joiner(jsons2arrays(data, headers), separator)
-);
-
-export const string2csv = ((data, headers, separator) =>
-  (headers) ? `${headers.join(separator)}\n${data}`: data
-);
-
-export const toCSV = (data, headers, separator) => {
- if (isJsons(data)) return jsons2csv(data, headers, separator);
- if (isArrays(data)) return arrays2csv(data, headers, separator);
- if (typeof data ==='string') return string2csv(data, headers, separator);
- throw new TypeError(`Data should be a "String", "Array of arrays" OR "Array of objects" `);
+  return foundValue === undefined ? '' : foundValue;
 };
 
-export const buildURI = ((data, uFEFF, headers, separator) => {
+export const elementOrEmpty = element =>
+  element || element === 0 ? element : '';
+
+export const joiner = (data, separator = ',') => {
+  const filteredData = data.filter(e => e);
+  console.log(filteredData);
+  return filteredData
+    .map(row =>
+      row.map(element => '"' + elementOrEmpty(element) + '"').join(separator)
+    )
+    .join(`\n`);
+};
+
+export const arrays2csv = (data, headers, separator) =>
+  joiner(headers ? [headers, ...data] : data, separator);
+
+export const jsons2csv = (data, headers, separator) =>
+  joiner(jsons2arrays(data, headers), separator);
+
+export const string2csv = (data, headers, separator) =>
+  headers ? `${headers.join(separator)}\n${data}` : data;
+
+export const toCSV = (data, headers, separator) => {
+  if (isJsons(data)) return jsons2csv(data, headers, separator);
+  if (isArrays(data)) return arrays2csv(data, headers, separator);
+  if (typeof data === 'string') return string2csv(data, headers, separator);
+  throw new TypeError(
+    `Data should be a "String", "Array of arrays" OR "Array of objects" `
+  );
+};
+
+export const buildURI = (data, uFEFF, headers, separator) => {
   const csv = toCSV(data, headers, separator);
   const type = isSafari() ? 'application/csv' : 'text/csv';
-  const blob = new Blob([uFEFF ? '\uFEFF' : '', csv], {type});
+  const blob = new Blob([uFEFF ? '\uFEFF' : '', csv], { type });
   const dataURI = `data:${type};charset=utf-8,${uFEFF ? '\uFEFF' : ''}${csv}`;
 
   const URL = window.URL || window.webkitURL;
 
-  return (typeof URL.createObjectURL === 'undefined')
+  return typeof URL.createObjectURL === 'undefined'
     ? dataURI
     : URL.createObjectURL(blob);
-});
-
+};
