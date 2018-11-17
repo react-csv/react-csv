705a70f33ba748027aa76a68d865dea6ada5c89d
NandoTheessen <nando.theessen@gmail.com>
2018-11-17 22:06:48 +0100

    Revert "remove console.log"
    This reverts commit 30eeca19ad612327d71bc83416a7b26cb1e6d9c1.

--- src/core.js
+++ src/core.js
@@ -56,6 +56,7 @@ export const elementOrEmpty = element =>
 
 export const joiner = (data, separator = ',') => {
   const filteredData = data.filter(e => e);
+  console.log(filteredData);
   return filteredData
     .map(row =>
       row.map(element => '"' + elementOrEmpty(element) + '"').join(separator)
